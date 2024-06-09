5e810bf4aa10552f1444d3dbd255b55cf86dc1c0
mar <137989121+mar5608@users.noreply.github.com>
2024-06-09 16:00:47 +0900

    自己流コーディング完了

--- js/script.js
+++ js/script.js
@@ -8,16 +8,24 @@ jQuery("#js-drawer-icon").on("click", function (e) {
   jQuery("#js-drawer-content").toggleClass("is-checked");
 });
 
-jQuery(".js-accordion").on("click", function (e) {
+/** スムーススクロール*/
+/** drawer 制御 */
+jQuery('#js-drawer-content a[href^="#"]').on("click", function (e) {
   e.preventDefault();
+  jQuery("#js-drawer-icon").removeClass("is-checked");
+  jQuery("#js-drawer-content").removeClass("is-checked");
+});
 
-  if (jQuery(this).parent().hasClass("is-open")) {
-    // thisで押した要素を取得する
-    jQuery(this).parent().removeClass("is-open");
-    // スライドしながら戻る
-    jQuery(this).next().slideUp();
-  } else {
-    jQuery(this).parent().addClass("is-open");
-    jQuery(this).next().slideDown();
-  }
+jQuery('a[href^="#"]').on("click", function (e) {
+  const speed = 300;
+  const id = jQuery(this).attr("href");
+  const target = jQuery("#" == id ? "html" : id);
+  const position = jQuery(target).offset().top;
+  jQuery("html,body").animate(
+    {
+      scrollTop: position,
+    },
+    speed,
+    "swing" // swing or liner
+  );
 });
