4f1bc8448dec2c018b0e129266edb2be4d7577e1
mar <137989121+mar5608@users.noreply.github.com>
2024-06-15 17:28:35 +0900

    スムーススクロール対応中（PC版OK,SP版NG）

--- js/script.js
+++ js/script.js
@@ -12,20 +12,21 @@ jQuery("#js-drawer-icon").on("click", function (e) {
 /** drawer 制御 */
 jQuery('#js-drawer-content a[href^="#"]').on("click", function (e) {
   e.preventDefault();
+  console.log("メソッドが正しく呼び出されました。");
   jQuery("#js-drawer-icon").removeClass("is-checked");
   jQuery("#js-drawer-content").removeClass("is-checked");
 });
 
-jQuery('a[href^="#"]').on("click", function (e) {
-  const speed = 300;
-  const id = jQuery(this).attr("href");
-  const target = jQuery("#" == id ? "html" : id);
-  const position = jQuery(target).offset().top;
-  jQuery("html,body").animate(
-    {
-      scrollTop: position,
-    },
-    speed,
-    "swing" // swing or liner
-  );
-});
+// jQuery('a[href^="#"]').on("click", function (e) {
+//   const speed = 300;
+//   const id = jQuery(this).attr("href");
+//   const target = jQuery("#" == id ? "html" : id);
+//   const position = jQuery(target).offset().top;
+//   jQuery("html,body").animate(
+//     {
+//       scrollTop: position,
+//     },
+//     speed,
+//     "swing" // swing or liner
+//   );
+// });
